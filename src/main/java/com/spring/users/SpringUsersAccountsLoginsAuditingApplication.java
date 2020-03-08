93bb55f015202fe916c83bf7343678f2d11881e4
ljuboss <ljubos@t-com.me>
2020-03-08 22:06:27 +0100

    first commit

---
+++ src/main/java/com/spring/users/SpringUsersAccountsLoginsAuditingApplication.java
@@ -0,0 +1,13 @@
+package com.spring.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsersAccountsLoginsAuditingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUsersAccountsLoginsAuditingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/users/SpringUsersAccountsLoginsAuditingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUsersAccountsLoginsAuditingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
